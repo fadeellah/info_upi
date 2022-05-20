946a455e77ec0cad55b835cf39b9fec5fc20e8f5
fadeellah <79691225+fadeellah@users.noreply.github.com>
2022-05-20 17:58:06 +0700

    Update Fasilitas.dart

--- lib/Fasilitas.dart
+++ lib/Fasilitas.dart
@@ -3,9 +3,18 @@ import 'package:flutter/widgets.dart';
 class Fasilitas extends StatelessWidget {
   const Fasilitas({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('fasilitas!'),
-    );
+   Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+            onTap: () {},
+            leading: Image.network(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+            trailing: const Icon(Icons.more_vert),
+            title: const Text('Sport Hall'),
+            subtitle: const Text("Fasilitas Olahraga Badminton dan Volly"),
+            tileColor: Colors.white70),
+                
+            );
+    
   }
 }
